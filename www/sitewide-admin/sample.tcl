598b72b424deee8447be78c915e053520bc63ba6
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-04-07 12:53:59 +0000

    Make use of uniform styling based on adp:icon

--- www/sitewide-admin/sample.tcl
+++ www/sitewide-admin/sample.tcl
@@ -37,28 +37,35 @@ set CSS_URL urn:ad:css:bootstrap-icons
 template::head::add_css -href $CSS_URL
 
 set content [subst {
-    <p> As image <img src="$URL/bootstrap.svg" alt="Bootstrap" width="32" height="32">
-    <p>As font + class: <br>
+    <h3>Using an Bootstrap Icons as SVG image</h3>
+    <p>Bootstrap <img src="$URL/bootstrap.svg" alt="Bootstrap" width="32" height="32">
+
+    <h3>Using an Bootstrap Icons as fonts (sample)</h3>
+                                           
     <blockquote class="mx-4">
     GitHub <i class="bi-github" role="img" aria-label="GitHub"></i><br>
-    pencil-square <i class="bi bi-pencil-square"></i><br>
     archive <i class="bi bi-archive"></i><br>
-    trash <i class="bi bi-trash"></i><br>
+    arrow-up-circle <i class="bi bi-arrow-up-circle"></i><br>
+    arrow-up-right-square <i class="bi bi-arrow-up-right-square"></i><br>
+    arrow-up-square <i class="bi bi-arrow-up-square"></i><br>
+    box-arrow-up-right <i class="bi bi-box-arrow-up-right"></i><br>
     clock-history <i class="bi bi-clock-history"></i><br>
+    cloud <i class="bi bi-cloud"></i><br>
+    envelope <i class="bi bi-envelope"></i><br>
     eye <i class="bi bi-eye"></i><br>
     eyeglasses <i class="bi bi-eyeglasses"></i><br>
     file-earmark <i class="bi bi-file-earmark"></i><br>
     files <i class="bi bi-files"></i><br>
-    envelope <i class="bi bi-envelope"></i><br>
-    upload <i class="bi bi-upload"></i><br>
-    info-square <i class="bi bi-info-square"></i><br>
     folder <i class="bi bi-folder"></i><br>
-    arrow-up-circle <i class="bi bi-arrow-up-circle"></i><br>
-    arrow-up-square <i class="bi bi-arrow-up-square"></i><br>
-    arrow-up-right-square <i class="bi bi-arrow-up-right-square"></i><br>
     folder-symlink <i class="bi bi-folder-symlink"></i><br>
-    cloud <i class="bi bi-cloud"></i><br>
+    info-square <i class="bi bi-info-square"></i><br>
+    pencil-square <i class="bi bi-pencil-square"></i><br>
+    trash <i class="bi bi-trash"></i><br>
+    upload <i class="bi bi-upload"></i><br>
     </blockquote>
+
+    <h3>Resource info for Bootstrap Icons</h3>
+    
     <p> <table>[join [lmap {k v} $resource_info {string cat <tr><td><i>$k ":</td><td> " $v</td></tr>}]]</table>
 }]
 
